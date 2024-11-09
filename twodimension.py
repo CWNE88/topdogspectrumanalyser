fa5df1cd100640b3902480bcbd93c4839bca3b1c
cwne88 <paul.stanley@topdognetworks.com.au>
2024-11-10 08:25:59 +1000

    peak hold work

--- twodimension.py
+++ twodimension.py
@@ -1,3 +1,6 @@
+
+
+            
 #!/bin/python3
 import sys
 import numpy as np
@@ -7,8 +10,8 @@ import pyqtgraph as pg
 class TwoD(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-        print ("TwoD init")
-        self.setWindowTitle("Top Dog Spectrum Analyser")
+        print("TwoD init")
+        
         self.setGeometry(100, 500, 1500, 600)
 
         # Create the plot widget
@@ -16,12 +19,17 @@ class TwoD(QtWidgets.QWidget):
         self.power_levels = None
         self.frequency_bins = None
         self.max_hold_levels = None
+
+        self.peak_search_frequency = None
+        self.peak_search_power = None
+
         self.max_hold_enabled = False
+        self.peak_search_enabled = False
 
         self.curve = self.widget.plot(pen='g', name='Current Power Levels')
         self.max_hold_curve = self.widget.plot(pen='y', alpha=0.5, name='Max Hold Levels')
 
-        self.widget.setTitle("Spectrum Analyser")
+        
         self.widget.setLabel('left', 'Amplitude (dBm)')
         self.widget.setLabel('bottom', 'Frequency (MHz)')
         self.widget.setYRange(-100, -10)
@@ -32,23 +40,48 @@ class TwoD(QtWidgets.QWidget):
         self.paused = False
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.update_plot)
-        #self.timer.start(20)
 
-    def update_widget_data(self, power_levels, max_hold_levels, frequency_bins):
+        self.peak_search_label = None   
+
+    def update_widget_data(self, power_levels, max_hold_levels, frequency_bins, peak_search_enabled):
         if power_levels is not None and max_hold_levels is not None and frequency_bins is not None:
             self.power_levels = power_levels
             self.max_hold_levels = max_hold_levels
             self.frequency_bins = frequency_bins
+            self.peak_search_enabled = peak_search_enabled
 
     def set_max_hold_enabled(self, max_hold_enabled):
         self.max_hold_enabled = max_hold_enabled
-            
+
+    def set_peak_search_enabled(self, peak_search_enabled, peak_search_frequency, peak_search_power):
+        self.peak_search_enabled = peak_search_enabled
+        self.peak_search_frequency = peak_search_frequency
+        self.peak_search_power = peak_search_power
+
     def update_plot(self):
         if self.power_levels is not None and self.frequency_bins is not None:
             self.curve.setData(self.frequency_bins / 1e6, self.power_levels)  
-            #print (self.max_hold_enabled)
-            if self.max_hold_enabled == True:
+            if self.max_hold_enabled:
                 self.max_hold_curve.setData(self.frequency_bins / 1e6, self.max_hold_levels)
             else:
                 self.max_hold_curve.clear()
 
+            
+            if self.peak_search_enabled:
+                peak_search_text = (
+                    f"<span style='color: green;background-color: black;'>Peak</span> <br>"
+                    f"<span style='color: white;background-color: black;'>{self.peak_search_frequency/1e6:.2f} MHz</span><br>"
+                    f"<span style='color: white; background-color: black;'>{self.peak_search_power:.2f} dB</span><br>"
+                )
+
+                if self.peak_search_label is None:
+                    self.peak_search_label = pg.TextItem(peak_search_text)
+                    self.peak_search_label.setHtml(peak_search_text)
+                    self.widget.addItem(self.peak_search_label)
+
+                self.peak_search_label.setHtml(peak_search_text)
+                self.peak_search_label.setPos(self.peak_search_frequency / 1e6, self.widget.viewRange()[1][1])
+            else:
+                if self.peak_search_label is not None:
+                    self.widget.removeItem(self.peak_search_label)
+                    self.peak_search_label = None
