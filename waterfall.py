813f1d9a94b6f5a450a8b385ba273798d07922bf
cwne88 <paul.stanley@topdognetworks.com.au>
2024-11-24 11:08:45 +1000

    history change

--- waterfall.py
+++ waterfall.py
@@ -35,7 +35,7 @@ class Waterfall(QtWidgets.QWidget):
         self.frequency_bins = None
         self.waterfall_array = None
         self.initialised = False
-        self.history_amount = 500
+        self.history_amount = 900
         self.min_level = -80
         self.max_level = -60
 
